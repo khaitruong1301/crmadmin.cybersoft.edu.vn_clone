47ef50d4d5071bf02d3ec519e355c18504dd4329
hoang <hoang.nhh1991@gmail.com>
2021-12-28 15:02:21 +0700

    Test parse token controller ok

---
+++ SoloDevApp.Api/Controllers/TestParseTokenController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SoloDevApp.Service.Constants;
+using SoloDevApp.Service.Services;
+using SoloDevApp.Service.ViewModels;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SoloDevApp.Api.Controllers
+{
+    [Route("api/aparsetoken")]
+    [ApiController]
+
+
+    public class TestParseTokenController : ControllerBase
+    {
+
+       
+
+        [HttpGet]
+        //[Authorize(Roles = "VIEW_ROLE")]
+        public async Task<IActionResult> Get(string tokenstring)
+        {
+            tokenstring = tokenstring.Replace("Bearer ", "");
+            //var stream = Encoding.ASCII.GetBytes("CYBERSOFT2020_SECRET");
+            JwtSecurityToken token = new JwtSecurityTokenHandler().ReadJwtToken(tokenstring);
+            var email = token.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value;
+
+            return new ResponseEntity(StatusCodeConstants.OK, email);
+        }
+
+    }
+}
