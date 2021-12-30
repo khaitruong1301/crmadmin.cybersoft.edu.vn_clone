51f35e5f3bbbc311a85116cab97fc4ab8294e1c4
hoang <hoang.nhh1991@gmail.com>
2021-12-30 15:36:55 +0700

    Add new modal class in repo for view table

--- SoloDevApp.Repository/Models/BaiHoc_TaiLieu_Link_TracNghiem.cs
+++ SoloDevApp.Repository/Models/BaiHoc_TaiLieu_Link_TracNghiem.cs
@@ -13,4 +13,39 @@
         public string MaLoaiBaiHoc { get; set; }
         public string GhiChu { get; set; }
     }
+
+    public class TaiLieu
+    {
+        public int Id { set; get; }
+        public string TieuDe { set; get; }
+        public string BiDanh { get; set; }
+        public string NoiDung { get; set; }
+        public string MoTa { get; set; }
+        public string GhiChu { set; get; }
+    }
+
+    public class Video : TaiLieu
+    {
+        public string Vimeo { set; get; }
+        public string ThoiLuong { get; set; }
+    }
+
+    public class TaiLieuBaiHoc : TaiLieu
+    {
+    }
+
+    public class TaiLieuDocThem : TaiLieu
+    {
+
+    }
+
+    public class TaiLieuBaiTap : TaiLieu
+        {}
+    public class TaiLieuProjectLamThem : TaiLieu
+    { }
+
+    public class TracNghiem : TaiLieu
+    {
+    }
+    public class VideoFPT : Video { }
 }
